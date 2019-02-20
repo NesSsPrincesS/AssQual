1e13b6c1bb523ad205e1cb0058a8de4057c57259
Jane <Jane-mary_love@hotmail.com>
2019-02-20 11:27:26 -0500

    ajout methode lecture plat et quelques modifications

--- src/tp1Partie2/ChezBarette.java
+++ src/tp1Partie2/ChezBarette.java
@@ -19,10 +19,11 @@ public class ChezBarette {
 	private final static String MESSAGE_ERREUR_FORMAT_FICHIER = "Le fichier ne respecte pas le format demandé ! ";
 	private final static String MESSAGE_BIENVENUE = "Bienvenue chez Barette!\nFactures:\n";
 
+	private static ArrayList<Commande> commandes = new ArrayList<Commande>();
+	private static ArrayList<Plats> listePlats = new ArrayList<Plats>();
+	private static boolean fichierOk = true;
+
 	public static void main(String[] args) {
-		ArrayList<Commande> commandes = new ArrayList<Commande>();
-		ArrayList<Plats> listePlats = new ArrayList<Plats>();
-		boolean fichierOk = true;
 
 		BufferedReader fic = new BufferedReader(new InputStreamReader(System.in));
 		System.out.println(MESSAGE_ENTRER_NOM_FICHIER);
@@ -53,11 +54,52 @@ public class ChezBarette {
 			List<String> listeCommandes = listeLignesFichier.subList(listeLignesFichier.indexOf("Commandes :") + 1,
 					listeLignesFichier.indexOf("Fin"));
 			
-
-		}else {
+			lecturePlats(listeCommandes, listeCommandes, listeCommandes);
+		} else {
 			System.out.println(MESSAGE_ERREUR_NOM_FICHIER);
 		}
 
+	}
+
+	public static void lecturePlats(List<String> listeNomPlats, List<String> listeCommandes, List<String> listeClients) {
+		
+		for (String string : listeNomPlats) {
+			String nomDuPlat = string.substring(0, string.indexOf(" "));
+			String prix = string.substring(string.indexOf(" "));
+			listePlats.add(new Plats(nomDuPlat, Double.parseDouble(prix)));
+		}
+		for (String string : listeCommandes) {
+			String nomLu = string.substring(0, string.indexOf(" "));
+			String nomPlat = string.substring(string.indexOf(" ") + 1, string.lastIndexOf(" "));
+			String qteLu = string.substring(string.lastIndexOf(" ") + 1);
+			Plats plat = Commande.trouverPlat(listePlats, nomPlat);
+			if (plat != null && listeClients.contains(nomLu)) {
+				commandes.add(new Commande(nomLu, Integer.parseInt(qteLu), plat));
+			} else {
+				System.out.println(MESSAGE_ERREUR_FORMAT_FICHIER);
+				fichierOk = false;
+				break;
+			}
+		}
+		if (fichierOk) {
+			double[] totalParClient = new double[listeClients.size()];
+			for (Commande commandeCourante : commandes) {
+				totalParClient[listeClients.indexOf(commandeCourante.nomClient)] += (commandeCourante.plat.getPrix()
+						* commandeCourante.qte);
+			}
+			String allText = MESSAGE_BIENVENUE;
+			for (String client : listeClients) {
+				allText += client + " :  " + totalParClient[listeClients.indexOf(client)] + " $\n";
+			}
+			System.out.println(allText);
+			try {
+				BufferedWriter writer = Files.newBufferedWriter(new File("Factures.txt").toPath());
+				writer.write(allText);
+				writer.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 }
