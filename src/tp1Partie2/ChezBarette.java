b6fcaa0b8b2aa8f9fe5c9145e35185ffa6280822
Jane <Jane-mary_love@hotmail.com>
2019-02-20 11:21:04 -0500

    Verifier si le fichier exist

--- src/tp1Partie2/ChezBarette.java
+++ src/tp1Partie2/ChezBarette.java
@@ -12,7 +12,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ChezBarette {
-	
+
 	private final static String MESSAGE_ENTRER_NOM_FICHIER = "Entrez le nom du fichier à lire: ";
 	private final static String MESSAGE_ERREUR_NOM_FICHIER = "Erreur le fichier n'existe pas";
 	private final static String MESSAGE_ERREUR_ENTREE_SORTI = "\nUne erreur d'entrée-sortie" + " est survenue.";
@@ -24,19 +24,40 @@ public class ChezBarette {
 		ArrayList<Plats> listePlats = new ArrayList<Plats>();
 		boolean fichierOk = true;
 
-		BufferedReader fic = new BufferedReader( new InputStreamReader( System.in ) );
-		System.out.println( MESSAGE_ENTRER_NOM_FICHIER);
+		BufferedReader fic = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println(MESSAGE_ENTRER_NOM_FICHIER);
 
 		String nomFichier = "";
 		try {
 			nomFichier = fic.readLine();
-		} catch ( IOException errIO ) {
+		} catch (IOException errIO) {
 			System.out.println(MESSAGE_ERREUR_ENTREE_SORTI);
 			errIO.printStackTrace();
 		}
-		Path cheminFichier = new File( nomFichier ).toPath();
+		Path cheminFichier = new File(nomFichier).toPath();
 
-		
+		if (Files.exists(cheminFichier)) {
+			Charset charset = Charset.defaultCharset();
+			List<String> listeLignesFichier = null;
+			try {
+				listeLignesFichier = Files.readAllLines(cheminFichier, charset);
+			} catch (IOException e) {
+				System.out.println(MESSAGE_ERREUR_FORMAT_FICHIER);
+				fichierOk = false;
+				e.printStackTrace();
+			}
+			List<String> listeClients = listeLignesFichier.subList(listeLignesFichier.indexOf("Clients :") + 1,
+					listeLignesFichier.indexOf("Plats :"));
+			List<String> listeNomPlats = listeLignesFichier.subList(listeLignesFichier.indexOf("Plats :") + 1,
+					listeLignesFichier.indexOf("Commandes :"));
+			List<String> listeCommandes = listeLignesFichier.subList(listeLignesFichier.indexOf("Commandes :") + 1,
+					listeLignesFichier.indexOf("Fin"));
+			
+
+		}else {
+			System.out.println(MESSAGE_ERREUR_NOM_FICHIER);
+		}
+
 	}
 
 }
